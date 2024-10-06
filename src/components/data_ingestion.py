8347781f32abeecdc7707e05f0c802efa496dd4d
Lokesh Kumar Saini <lokesh726888@gmail.com>
2024-10-06 15:09:01 +0530

    Implemented model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,7 @@ from dataclasses import dataclass
 import os
 
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import modeltrainer
 
 
 def create_engine(server,database):
@@ -62,4 +63,6 @@ if __name__=='__main__':
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    model_train=modeltrainer()
+    print(model_train.initiate_model_train(train_arr,test_arr))
